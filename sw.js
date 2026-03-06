5ef171b027fa9c1ff69e0fbc4bab4216c4bc1350
lucatrzich <lucaterras@gmail.com>
2026-03-06 19:00:00 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,8 @@
-self.addEventListener('install', (e) => {
-  self.skipWaiting();
+self.addEventListener('install', (event) => {
+  console.log('Service Worker instalado');
 });
 
-self.addEventListener('fetch', (e) => {
-  // Solo passthrough
+self.addEventListener('fetch', (event) => {
+  // Esto permite que la app funcione offline o cargue rápido
+  event.respondWith(fetch(event.request));
 });
